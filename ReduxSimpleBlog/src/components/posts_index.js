703385fec57d73da808a6294d3aaf9758bcfa647
dd <ddresearch@shaw.ca>
2017-07-30 18:07:39 -0700

    PostsNew form

--- ReduxSimpleBlog/src/components/posts_index.js
+++ ReduxSimpleBlog/src/components/posts_index.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -10,10 +12,38 @@ class PostsIndex extends Component {
 		this.props.fetchPosts();
 	}
 
+	renderPosts() {
+		//  We have an object here instead of an array
+		//  so use the lodash function to help us
+		return _.map(this.props.posts, post => {
+			return (
+				<li className="list-group-item" key={post.id} >
+					{post.title}
+				</li>
+			)
+		})
+	}
+
 	render() {
 		return (
-			<div>PostsIndex</div>
+			<div>
+				<div className="text-xs-right">
+					<Link className="btn btn-primary" to="/posts/new">
+						Add a Post
+					</Link>
+				</div>
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
+			</div>
 		)
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		posts: state.posts
 	}
 }
 
@@ -21,6 +51,6 @@ class PostsIndex extends Component {
 //  mapDispatchToProps.  It is all happening behind the
 //  scenes
 export default connect(
-	null,
+	mapStateToProps,
 	{ fetchPosts }
 )(PostsIndex);
---
+++ ReduxSimpleBlog/src/components/posts_new.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+
+	renderTitleField(field) {
+		return (
+			<div>
+				<input
+					type="text"
+					{...field.input}
+				/>
+			</div>
+		)
+	}
+
+	render() {
+		return (
+			<form>
+				<Field
+					name="title"
+					component={this.renderTitleField}
+				/>
+			</form>
+		)
+	}
+}
+
+export default reduxForm(
+	{
+		//  Name of the form
+		form: 'PostsNewForm'
+	}
+)(PostsNew)
