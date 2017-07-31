7a1f2d9f2f7ed1013a1d77dd27f26865e01db4e8
dd <ddresearch@shaw.ca>
2017-07-30 22:16:57 -0700

    PostsNew validation

--- ReduxSimpleBlog/src/components/posts_new.js
+++ ReduxSimpleBlog/src/components/posts_new.js
@@ -3,31 +3,86 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 
-	renderTitleField(field) {
+	renderField(field) {
+		const  { meta : { touched, error } } = field;
+
+		const className = `form-group ${touched &&
+        				error ? 'has-danger' : ''}`;
 		return (
-			<div>
+			<div className={className}>
+				<label>{field.label}</label>
 				<input
+					className="form-control"
 					type="text"
 					{...field.input}
 				/>
+				<div className="text-help">
+					{touched ? error : ''}
+        		</div>
 			</div>
 		)
 	}
 
+	onSubmit(values) {
+		console.log(values);
+	}
+
 	render() {
+		const { handleSubmit } = this.props;
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<Field
+					label="Title"
 					name="title"
-					component={this.renderTitleField}
+					component={this.renderField}
 				/>
+				<Field
+					label="Categories"
+					name="categories"
+					component={this.renderField}
+				/>
+				<Field
+					label="Post Content"
+					name="content"
+					component={this.renderField}
+				/>
+				<button 
+					type="submit"
+					className="btn btn-primary">
+						Submit
+				</button>
 			</form>
 		)
 	}
 }
 
+//  Validation of the form
+function validate(values) {
+	const errors = {};
+
+	if (!values.title) {
+		errors.title = "Enter a title!";
+	}
+
+	if (!values.categories) {
+		errors.categories = "Enter some categories!";
+	}
+
+	if (!values.content) {
+		errors.content = "Enter some content!";
+	}
+	//  Empty errors means good.  Else redux-form assumes
+	//  form is invalid
+	return errors;
+}
+
 export default reduxForm(
 	{
+		//  Pass the validation function to the form
+		//  Redux-form takes care of calling this onSubmit
+		//  or when the enter key is pressed, etc.
+		validate,
 		//  Name of the form
 		form: 'PostsNewForm'
 	}
